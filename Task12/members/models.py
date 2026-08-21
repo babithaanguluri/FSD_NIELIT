74e7806f85c2c321e20ccba671aac7241fa4ffeb
Your Name <you@example.com>
2026-08-22 00:54:03 +0530

    Django folder structure and project creation

---
+++ Task12/members/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class UserData(models.Model):
+  firstname = models.CharField(max_length=100)
+  lastname = models.CharField(max_length=100)
+  email =models.EmailField(unique=True)
+  password=models.CharField(max_length=128)
+  def __str__(self):
+    return self.email
